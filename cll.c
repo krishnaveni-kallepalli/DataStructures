ddf97c4297755e10b1addeb607a86aeaf33de284
krish <krishnaveni.k@thundersoft.com>
2022-11-17 17:53:34 +0530

    17/11/2022

--- cll.c
+++ cll.c
@@ -58,6 +58,50 @@ void insert_end(int data)
 	ptr->link  = new;
 	new->link = head;	
 }
+void insert_middle(int data)
+{
+	struct node *new = malloc(sizeof(struct node *));
+	struct node *p,*q;
+	p = head;
+	q = head;
+	new->data = data;
+	new->link = NULL;
+	while(q != NULL && q->link != head)
+	{
+		q = q->link->link;
+		p = p->link;
+	}
+	printf("Middle node is : %d\n",p->data);
+	new->link  = p->link;
+	p->link  = new;
+}
+void insert_pos(int data,int pos)
+{
+	struct node *new = malloc(sizeof(struct node *));
+	struct node *ptr = head;
+	new->data = data;
+	new->link = NULL;
+	pos--;
+	while(pos != 1)
+	{
+		ptr = ptr->link;
+		pos--;
+	}
+	new->link  = ptr->link;
+	ptr->link  = new;
+}
+void find_middle()
+{
+	struct node *p,*q;
+	q = head->link;
+	p = head;
+	while(q!= NULL && q->link != head)
+	{
+		q = q->link->link;
+		p = p->link;
+	}
+	printf("Middle Node is : %d\n",p->data);
+}
 void display()
 {
 	struct node *ptr = head;
@@ -71,7 +115,6 @@ void display()
 		{
 			printf("%d\n",ptr->data);
 			ptr = ptr->link;
-		//	printf("%d\n",ptr->data);
 		}
 		printf("%d\n",ptr->data);
 	}
@@ -92,7 +135,7 @@ int main()
 	display();
 	*/
 	struct node *ptr = head;
-	int n,i,data,d,ch;
+	int n,i,data,d,ch,pos;
 	printf("Enter no of nodes :\n");
 	scanf("%d",&n);
 	printf("Enter data:\n");
@@ -103,7 +146,7 @@ int main()
 	}
 	printf("Display\n");
 	display();
-	printf("1.Insert at begining\n");
+	printf("1.Insert at begining\n2.Insert at end\n3.Insert at middle\n4.Insert at position\n5.Find middle\n");
 	printf("Enter choice:");
 	scanf("%d",&ch);
 	switch(ch)
@@ -114,12 +157,33 @@ int main()
 			insert_begin(data);
 			printf("Display:\n");
 			display();
+			break;
 		case 2:
 			printf("Enter data:");
 			scanf("%d",&data);
 			insert_end(data);
 			printf("Display:\n");
 			display();
+			break;
+		case 3:
+			printf("Enter data:");
+			scanf("%d",&data);
+			insert_middle(data);
+			printf("Display:\n");
+			display();
+			break;
+		case 4:
+			printf("Enter data:");
+			scanf("%d",&data);
+			printf("Enter pos:");
+			scanf("%d",&pos);
+			insert_pos(data,pos);
+			printf("Display:\n");
+			display();
+			break;
+		case 5:
+			find_middle(data);
+			break;
 		default:exit(0);
 	}
 }
